f0314fcb5f8a66b3b79f79ffe22299e4a401ad4d
ezaatama <ezatama33@gmail.com>
2024-07-10 15:48:54 +0700

    update soal 2

--- bin/soal_algoritma/soal_2.dart
+++ bin/soal_algoritma/soal_2.dart
@@ -2,7 +2,7 @@ import 'package:coding_algoritma/soal_algoritma/soal_1.dart';
 import 'package:coding_algoritma/soal_algoritma/soal_2.dart';
 
 void main(List<String> arguments) {
-  final List<int> numbers = [];
+  final List<int> numbers = [1, 3, 5, 7, 2, 11];
   int maxNumbers = findMaxNumber(numbers);
   print('Max number dari array "$numbers" adalah $maxNumbers');
 }
